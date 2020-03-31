afb6cca970084c02d41bbb0149670b464c8c88d1
Akhmad Fauzy Epriliawan <ak.fauzy81@gmail.com>
2020-03-31 17:53:40 +0700

    Prototype 1 : Add List dan Select List

--- KasirAmeliaFC/Form1.cs
+++ KasirAmeliaFC/Form1.cs
@@ -22,7 +22,66 @@ namespace KasirAmeliaFC
         {
             if (e.KeyChar == (char)Keys.Enter)
             {
+                //String par;
                 //label2.Text = textBox1.Text;
+                try
+                {
+                    string cs = @"server=localhost;userid=root;password=VbsMac81;database=kasirameliafc";
+
+                    var con = new MySqlConnection(cs);
+                    var con1 = new MySqlConnection(cs);
+                    con.Open();
+                    con1.Open();
+
+                    MySqlCommand check_barang = new MySqlCommand("SELECT * FROM cart WHERE (barang = @barang)", con);
+                    check_barang.Parameters.AddWithValue("@barang", textBox1.Text);
+                    MySqlDataReader reader = check_barang.ExecuteReader();
+                    if (reader.HasRows)
+                    {
+                        //KMessageBox.Show("aw");
+                        var sql = "UPDATE cart SET jumlah=jumlah+1 WHERE barang=@barc";
+                        var cmd = new MySqlCommand(sql, con1);
+                        cmd.Parameters.AddWithValue("@barc", textBox1.Text);
+                        cmd.Prepare();
+                        cmd.ExecuteNonQuery();
+                    }
+                    else
+                    {
+                        //MessageBox.Show("ew");
+                        var sql = "INSERT INTO cart(Barang, Jumlah) VALUES(@barc, @jum)";
+                        var cmd = new MySqlCommand(sql, con1);
+                        cmd.Parameters.AddWithValue("@barc", textBox1.Text);
+                        cmd.Parameters.AddWithValue("@jum", 1);
+                        cmd.Prepare();
+                        cmd.ExecuteNonQuery();
+                    }
+
+                    //var sql = string.Format("select * from listbarang where barcode='{0}'", textBox1.Text);
+                    //var cmd = new MySqlCommand(sql, con);
+                    //MySqlDataReader rdr = cmd.ExecuteReader();
+                    //Console.WriteLine($"{rdr.GetName(0),-4} {rdr.GetName(1),-10} {rdr.GetName(2),10}");
+                    //while (rdr.Read())
+                    //{
+                    //    int fl = 0;
+                    //    Console.WriteLine(fl);
+                    //    Console.WriteLine($"{rdr.GetString(0),-4} {rdr.GetString(1),-10} {rdr.GetString(2),10}");
+                    //    fl++;
+                    //}
+
+                    DataTable dt = new DataTable();
+                    MySqlDataAdapter adapt = new MySqlDataAdapter("select cart.id, listbarang.nama, listbarang.harga, cart.jumlah, (cart.jumlah * listbarang.harga) as Total from cart join listbarang on cart.barang = listbarang.barcode order by cart.id asc", con1);
+                    adapt.Fill(dt);
+                    dataGridView1.DataSource = dt;
+
+                    con.Close();
+                    con1.Close();
+                    //Console.WriteLine($"MySQL version : {con.ServerVersion}");
+                }
+                catch (Exception ex)
+                {
+                    //toolStripStatusLabel2.Text = "Offline";
+                    MessageBox.Show(ex.Message);
+                }
                 textBox1.Clear();
                 textBox1.Focus();
             }
@@ -40,6 +99,7 @@ namespace KasirAmeliaFC
 
                 toolStripStatusLabel2.Text = "Online";
 
+                con.Close();
                 //Console.WriteLine($"MySQL version : {con.ServerVersion}");
             }
             catch (Exception ex)
--- KasirAmeliaFC/Form2.cs
+++ KasirAmeliaFC/Form2.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace KasirAmeliaFC
 {
@@ -20,6 +21,41 @@ namespace KasirAmeliaFC
         private void Form2_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void Form2_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            new Form1().Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string cs = @"server=localhost;userid=root;password=VbsMac81;database=kasirameliafc";
+
+                var con = new MySqlConnection(cs);
+                con.Open();
+
+                //toolStripStatusLabel2.Text = "Online";
+                var sql = "INSERT INTO listbarang(Barcode, Nama, Harga) VALUES(@barc, @name, @val)";
+                var cmd = new MySqlCommand(sql, con);
+
+                cmd.Parameters.AddWithValue("@barc", textBox1.Text);
+                cmd.Parameters.AddWithValue("@name", textBox2.Text);
+                cmd.Parameters.AddWithValue("@val", textBox3.Text);
+                cmd.Prepare();
+
+                cmd.ExecuteNonQuery();
+
+                con.Close();
+                //Console.WriteLine($"MySQL version : {con.ServerVersion}");
+            }
+            catch (Exception ex)
+            {
+                //toolStripStatusLabel2.Text = "Offline";
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
